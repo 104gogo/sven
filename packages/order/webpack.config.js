5264a09f93aea250ed75b57a422fb8663680f3a4
gogo <xiaotian.zhang@56qq.com>
2019-03-08 15:23:37 +0800

    feat: 添加babel-loader

--- packages/order/webpack.config.js
+++ packages/order/webpack.config.js
@@ -7,6 +7,20 @@ module.exports = {
     filename: '[name].js',
     path: __dirname + '/dist'
   },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /(node_modules|bower_components)/,
+  //       use: {
+  //         loader: 'babel-loader',
+  //         options: {
+  //           presets: ['@babel/preset-env']
+  //         }
+  //       }
+  //     }
+  //   ]
+  // },
   plugins: [
     new htmlPlugin({
       title: 'webpack order test',
